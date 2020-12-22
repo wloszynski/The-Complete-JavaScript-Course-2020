112040626b34a198151d75ae0cb5f37c23da934e
wloszynski <adrian@wloszynski.pl>
2020-12-22 10:25:15 +0100

    default parameters

---
+++ 10-Functions/default_parameters.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 899);
+createBooking('LH123', 2);
+createBooking('LH123', undefined, 1000);
