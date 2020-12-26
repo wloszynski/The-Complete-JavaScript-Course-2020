96ed9ba00d63b5448c44c6ef4fd3037f8c612721
wloszynski <adrian@wloszynski.pl>
2020-12-26 14:37:04 +0100

    sticky navigation

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -127,4 +127,17 @@ nav.addEventListener("mouseover", handleHover.bind(0.5));
 
 nav.addEventListener("mouseout", handleHover.bind(1));
 
+// Sticky navigation
+const initialCords = section1.getBoundingClientRect();
+
+window.addEventListener("scroll", function (e) {
+  console.log(initialCords);
+
+  if (window.scrollY > initialCords.top) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+});
+
 ///////////////////////////////////////////////////////////
