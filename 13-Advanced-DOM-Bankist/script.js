0f00193e484adc3eb5afd1c976ab45df5887f090
wloszynski <adrian@wloszynski.pl>
2020-12-27 14:10:53 +0100

    adding sticky nav

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -128,16 +128,52 @@ nav.addEventListener("mouseover", handleHover.bind(0.5));
 nav.addEventListener("mouseout", handleHover.bind(1));
 
 // Sticky navigation
-const initialCords = section1.getBoundingClientRect();
+// const initialCords = section1.getBoundingClientRect();
 
-window.addEventListener("scroll", function (e) {
-  console.log(initialCords);
+// window.addEventListener("scroll", function (e) {
+//   console.log(initialCords);
 
-  if (window.scrollY > initialCords.top) {
-    nav.classList.add("sticky");
-  } else {
+//   if (window.scrollY > initialCords.top) {
+//     nav.classList.add("sticky");
+//   } else {
+//     nav.classList.remove("sticky");
+//   }
+// });
+
+// Sticky navigation: Intersection Observer API
+
+// const obsCallBack = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   });
+// };
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallBack, obsOptions);
+// observer.observe(section1);
+
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else {
     nav.classList.remove("sticky");
   }
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+
+headerObserver.observe(header);
 
 ///////////////////////////////////////////////////////////
